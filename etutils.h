7ca120088b2c68b1f1b42bc251ca5c79322347ae
feri <github@fh.anderemails.de>
2020-12-19 00:21:29 +0100

    v3.8.1: fixed missing include <stdarg>.

---
+++ etutils.h
@@ -0,0 +1,17 @@
+#ifndef ETUTILS_H
+#define ETUTILS_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+//size_t float2char(char* buf, float number, uint8_t digits);
+//float char2float(const char* buf);
+
+//void throwError(const char *errMsg);
+
+void log(const char *msg);
+void logFloat(float f);
+void logInt(int i);
+
+
+#endif // ETUTILS_H
